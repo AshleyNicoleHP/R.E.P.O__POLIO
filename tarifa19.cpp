0b5bd0f95ea7dc9f790ee47ff45e6e843de1a82a
Pavlooo122 <00030325@uca.edu.sv>
2025-06-03 10:15:44 -0600

    prueva

---
+++ tarifa19.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int consumo, precio;
+
+    cout << "Ingrese el consumo de kWh" << endl;
+    cin >> consumo;
+
+    if (consumo < 100)
+    {
+        precio = consumo * 0.25;
+        cout << "Su tarifa es baja:" << precio << endl;
+    }
+
+    else if (100 > consumo < 200)
+    {
+        precio = consumo * 0.25;
+        cout << "Su tarifa es media:" << precio << endl;
+    }
+        else if (consumo > 200)
+    {
+        precio = consumo * 0.25;
+        cout << "Su tarifa es Alta:" << precio << endl;
+    }
+
+    return 0;
+}
