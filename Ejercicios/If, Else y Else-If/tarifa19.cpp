91e5cf664787c91d17ef643bb9824fee29efd3c0
AshleyNicoleHP <00078125@uca.edu.sv>
2025-06-05 22:09:31 -0600

    arreglado

--- Ejercicios/If, Else y Else-If/tarifa19.cpp
+++ Ejercicios/If, Else y Else-If/tarifa19.cpp
@@ -6,24 +6,24 @@ int main()
 {
     int consumo, precio;
 
-    cout << "Ingrese el consumo de kWh" << endl;
+    cout << "Ingrese el consumo de kWh " << endl;
     cin >> consumo;
 
     if (consumo < 100)
     {
         precio = consumo * 0.25;
-        cout << "Su tarifa es baja:" << precio << endl;
+        cout << "Su tarifa es baja: " << precio << endl;
     }
 
     else if (100 > consumo < 200)
     {
         precio = consumo * 0.25;
-        cout << "Su tarifa es media:aaaaaaaa" << precio << endl;
+        cout << "Su tarifa es media: " << precio << endl;
     }
         else if (consumo > 200)
     {
         precio = consumo * 0.25;
-        cout << "Su tarifa es Alta:" << precio << endl;
+        cout << "Su tarifa es Alta: " << precio << endl;
     }
 
     return 0;
